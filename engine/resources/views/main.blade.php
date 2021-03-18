860f36326e066c1764bfb1960b93697a86beceb8
Gleisson Neves <53718407+gdk46@users.noreply.github.com>
2021-03-18 07:34:09 -0300

    upgrade

---
+++ engine/resources/views/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>API-V1</title>
+</head>
+<body>
+    <main>
+        <h1>endpoints</h1>
+        <table border="1">
+            <tr>
+                <td></td>
+                <td>Recurso</td>
+                <td>URL</td>
+            </tr>
+            <tr>
+                <td>GET</td>
+                <td>READ ALL</td>
+                <td>http://127.0.0.1:8000/api/</td>
+            </tr>
+            <tr>
+                <td>POST</td>
+                <td>CREATE</td>
+                <td>http://127.0.0.1:8000/api/</td>
+            </tr>
+        </table>
+    </main>
+</body>
+</html>
